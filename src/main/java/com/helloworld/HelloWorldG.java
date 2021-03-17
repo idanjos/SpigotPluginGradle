462b2e4714f50c26198cec3824e803291c1ed372
idanjos <itda@ua.pt>
2021-03-17 20:48:32 +0000

    removed DV8 dep

--- src/main/java/com/helloworld/HelloWorldG.java
+++ src/main/java/com/helloworld/HelloWorldG.java
@@ -5,14 +5,11 @@ package com.helloworld;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-import javax.security.auth.login.LoginException;
-
 public class HelloWorldG extends JavaPlugin {
     @Override
     public void onEnable(){
 
-        Bot b = new Bot("Nzk5ODIwNjkwNDc1NzEyNTQy.YAJJEA.yIP2f-eOfrWEbRb9j2Lrv_llzBg");
+
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[HelloWorld] HelloWorld enabled!");
     }
 
